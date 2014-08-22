65c344184c1576d3137d0453f36675a95bebc0ef
Guest <praveenkh88@github.com>
2014-08-23 01:31:38 +0530

    initial commit

---
+++ homepi.js
@@ -0,0 +1,4 @@
+document.getElementById("b1").onclick = function(){document.getElementById("demo").innerHTML = "You Clicked On!!";};
+
+document.getElementById("b2").onclick = function(){document.getElementById("demo").innerHTML = "You Clicked Off!!";};
+
